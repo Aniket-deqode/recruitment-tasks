a8ba3fa8897d22ba84d82c9ee9c2e5fbeb645deb
Aniket-deqode <atikone@deqode.com>
2021-09-21 10:21:14 +0530

    feat:initialized swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 
@@ -7,6 +8,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService: ConfigService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Recruitment Task')
+    .setDescription('User Management')
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('PORT'));
 }
