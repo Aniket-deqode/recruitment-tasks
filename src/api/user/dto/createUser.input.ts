626ccfe829bc5af0564cc3abb7bf0e8dbb3ac27f
Aniket-deqode <atikone@deqode.com>
2021-09-21 10:24:01 +0530

    feat:added swagger decorators to dto schema

--- src/api/user/dto/createUser.input.ts
+++ src/api/user/dto/createUser.input.ts
@@ -1,15 +1,19 @@
 import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDTO {
+  @ApiProperty({ required: false, example: 'Aniket@deqode.com' })
   @IsEmail()
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ required: true, example: 'Aniket' })
   @IsString()
   @IsNotEmpty()
   givenName: string;
 
+  @ApiProperty({ required: true, example: 'Tikone' })
   @IsString()
   @IsNotEmpty()
   familyName: string;
