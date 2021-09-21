90cfa913b90c175d8d62ef829f4775c1c81c3270
Aniket-deqode <atikone@deqode.com>
2021-09-21 10:22:50 +0530

    feat:added swagger decorators to exposed api's

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -8,6 +8,15 @@ import {
   Delete,
 } from '@nestjs/common';
 import { BadRequestException } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiCreatedResponse,
+  ApiParam,
+} from '@nestjs/swagger';
 import { User } from 'src/api/user/schema/user.model';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dto/createUser.input';
@@ -18,6 +27,12 @@ import { UpdateUserDTO } from './dto/updateUser.input';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiTags('User')
+  @ApiOperation({ summary: 'Create User' })
+  @ApiCreatedResponse({ type: CreateUserDTO, description: 'User Created' })
+  @ApiBadRequestResponse({
+    description: 'User Already Exist with same Email',
+  })
   @Post('create')
   async create(@Body() Body: CreateUserDTO): Promise<User> {
     const newUser = Body;
@@ -37,6 +52,18 @@ export class UserController {
     }
   }
 
+  @ApiTags('User')
+  @ApiOperation({ summary: 'Update User' })
+  @ApiOkResponse({ type: CreateUserDTO, description: 'Update User Info' })
+  @ApiBadRequestResponse({
+    description: 'User Does Not Exist',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'User Id',
+    schema: { oneOf: [{ type: 'string' }, { type: 'integer' }] },
+  })
   @Put('update/:id')
   async update(
     @Param() { id }: UserIDDTO,
@@ -56,6 +83,18 @@ export class UserController {
     }
   }
 
+  @ApiTags('User')
+  @ApiOperation({ summary: 'Get User Info' })
+  @ApiResponse({ type: CreateUserDTO, description: 'User Information' })
+  @ApiBadRequestResponse({
+    description: 'User Does Not Exist',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'User Id',
+    schema: { oneOf: [{ type: 'string' }, { type: 'integer' }] },
+  })
   @Get(':id')
   async getUser(@Param() { id }: UserIDDTO): Promise<User> {
     try {
@@ -69,6 +108,18 @@ export class UserController {
     }
   }
 
+  @ApiTags('User')
+  @ApiOperation({ summary: 'Get User Info' })
+  @ApiCreatedResponse({ type: CreateUserDTO, description: 'User Deleted' })
+  @ApiBadRequestResponse({
+    description: 'User Does Not Exist',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'User Id',
+    schema: { oneOf: [{ type: 'string' }, { type: 'integer' }] },
+  })
   @Delete(':id')
   async delete(@Param() { id }): Promise<User> {
     try {
